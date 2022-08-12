b430663c0924c7b37721120a97d94355df5ca3bc
Dfxc <15779900432@163.com>
2022-08-12 14:54:09 +0800

    io流
    获取文件信息

--- io/src/com/banyan/file/FileInformation.java
+++ io/src/com/banyan/file/FileInformation.java
@@ -17,11 +17,17 @@ public class FileInformation {
      */
     public void info() {
         // 先创建对象获取信息
-        File file = new File("src\\folder\\test01.txt");
+//        File file = new File("src\\folder\\test01.txt");
+        File file = new File("D:\\Github-Project-workspace\\hsp_learning\\io\\src\\folder\\test01.txt");
 
         System.out.println("文件名 " + file.getName());
         System.out.println("绝对路径 " + file.getAbsolutePath());
         System.out.println("文件父目录 " + file.getParent());
-        System.out.println("文件大小（字节） " + file.length());
+        // 相对路径的文件对象，无法获取
+        System.out.println("文件大小（字节） " + file.length());// 文字3字节，字母数字1字节
+        System.out.println("文件是否存在 " + file.exists());// T
+        System.out.println("是否是一个文件 " + file.isFile());
+        System.out.println("是否是一个目录 " + file.isDirectory());
+
     }
 }
